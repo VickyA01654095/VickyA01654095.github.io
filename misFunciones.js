c9ee6a4f2c55b77df1b6b82617706362349cbbf0
Victoria Vazquez <65380353+VickyA01654095@users.noreply.github.com>
2022-02-22 22:50:27 -0600

    Delete misFunciones.js

--- misFunciones.js
+++
@@ -1,8 +0,0 @@
-var hola = "Me pondre en contacto"
-
-function procesarFormulario(){
-
-	var nom = document.getElementById("nombre").value;
-	document.getElementById("salida").innerHTML = hola + " " + nom;
-
-}
